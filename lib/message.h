9743ca8d9b88823ed2851aa6c60e9cd935cac6bf
Samuel Terra <samuelterra22@hotmail.com>
2019-09-24 17:58:35 -0300

    Define types for message

--- lib/message.h
+++ lib/message.h
@@ -1,18 +1,23 @@
-//
-// Created by samuel on 24/09/2019.
-//
-
 #ifndef MULTI_USER_SOCKETS_CHAT_MESSAGE_H
 #define MULTI_USER_SOCKETS_CHAT_MESSAGE_H
 
 #define MAX_MESSAGE_SIZE 200
 #define MAX_SENDER_SIZE 20
+
 #define TRUE 1
 #define FALSE 0
 
+enum MESSAGE_TYPE {
+    PRESENTATION_TYPE, NORMAL_TYPE, TERMINATE_TYPE
+};
+
+#include <time.h>
+
 struct Message {
-    char sender[MAX_SENDER_SIZE]; /* sender of message */
-    char message[MAX_MESSAGE_SIZE]; /* text message */
+    char sender[MAX_SENDER_SIZE];   /* sender of text */
+    char text[MAX_MESSAGE_SIZE];    /* text text */
+    struct tm tm;                   /* date value */
+    enum MESSAGE_TYPE type;         /* message type */
 };
 
 #endif //MULTI_USER_SOCKETS_CHAT_MESSAGE_H
